f64553407e6b452c29909ddcd261cf75a29cca26
irfanrizkiaditri <irfanrizkiaditri@gmail.com>
2025-11-11 01:38:19 +0700

    Delete js directory

--- js/cloud-config.js
+++
@@ -1,6 +0,0 @@
-// Cloudinary config (paste this file to js/cloud-config.js)
-// Replace values if needed. You already provided cloudName and uploadPreset.
-window.CLOUDINARY = {
-  cloudName: "dew39kqhy",
-  uploadPreset: "dew39kqhy"
-};
